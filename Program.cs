cde2bea75aa5976f15f345019273c5dea5bc5ff9
logikuma <logical.kumachan1004@gmail.com>
2017-08-27 15:13:24 +0900

    2番ピン点灯に成功

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PiTest2
 {
@@ -10,7 +11,24 @@ namespace PiTest2
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Raspberry Pi Test");
+
+            //とりあえず2番ピンでテスト
+            if (!Directory.Exists(@"/sys/class/gpio/gpio2"))
+            {
+                Console.WriteLine("open ping 2");
+
+                File.WriteAllText("/sys/class/gpio/export", "2");
+            }
+            else
+            {
+                Console.WriteLine("...pin is already open");
+            }
+
+            Console.WriteLine("...specifying direction of Pin 2 as OUT");
+            File.WriteAllText("/sys/class/gpio/gpio2/direction", "out");
+
+            File.WriteAllText("/sys/class/gpio/gpio2/value", "1");
         }
     }
 }
