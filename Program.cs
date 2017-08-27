8ba13fb214ee5b9f83d39d4f7718e442b36fc3b9
logikuma <logical.kumachan1004@gmail.com>
2017-08-27 14:28:10 +0900

    Add Comment

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace PiTest2
 {
     class Program
     {
+        /// <summary>
+        /// Application Entry
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
